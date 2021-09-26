b803335084b7e9d0d683a9784d1438f525033104
johannes <johannes@lgjt.xyz>
2021-09-26 20:41:58 +0200

    system works, just need to add a data base and a way to link multiple steam accounts to 1 discord account

--- webhook.py
+++ webhook.py
@@ -1,10 +1,30 @@
 from discord import Webhook, RequestsWebhookAdapter
+from datetime import datetime
+from discord.ext import commands
 
 import json
 import discord
 
 env = json.load(open("env.json"))
-webhook = Webhook.from_url(env["webhook"]["webhook_url"], adapter=RequestsWebhookAdapter())
 
-def new_user_added(discord_id): 
-    return
+bot = commands.Bot(command_prefix='#')
+
+intents = discord.Intents.default()
+intents.members = True
+
+STEAM_COMMUNITY_URL="https://steamcommunity.com/profiles/"
+
+async def new_user_added(discord_id, discord_username, steam_id): 
+
+    webhook = Webhook.from_url(env["webhook"]["webhook_url"], adapter=RequestsWebhookAdapter())
+    guild = webhook.guild
+    print(guild)
+
+    embed = discord.Embed(title=f"{discord_username} succesfully verified their accounts!")
+    embed.add_field(name="Discord ID", value=discord_id)
+    embed.add_field(name="Discord Username", value=discord_username)
+    embed.add_field(name="Discord Mention", value=f"<@{discord_id}>")
+    embed.add_field(name="Steam ID", value=steam_id)
+    embed.add_field(name="Stean Profile", value=f"{STEAM_COMMUNITY_URL}{steam_id}")
+    embed.set_footer(text=datetime.now().strftime('%Y-%m-%d %H:%M'))
+    webhook.send(embed=embed, username=env["name"]+"-hook")
--- webserver.py
+++ webserver.py
@@ -1,20 +1,22 @@
 from flask import Flask, render_template, request, jsonify
+from flask.helpers import make_response
 from flask_pymongo import PyMongo
+from werkzeug.utils import redirect
+
+from pysteamsignin.steamsignin import SteamSignIn
 
 import json
 import requests
 import discord_oauth
+import webhook
 
 from werkzeug.wrappers import response
 app = Flask(__name__, static_folder='static')
 env = json.load(open("env.json"))
 
-
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 @app.route('/discord')
 def discord():
@@ -22,16 +24,46 @@ def discord():
     if code:
         token = discord_oauth.get_access_token(code)["access_token"]
         user = discord_oauth.get_user(token)
+        
+        resp = make_response(redirect("/steam"))
+        resp.set_cookie("discord_id", value=user["id"])
+        resp.set_cookie("discord_username", value=user["username"]) 
 
-        return user
+        print(user)
+        return resp
     else:
         return render_template("views/discord.html", discord_url = env["oauth_discord"]["sign_in_url"])
 
-@app.route('/link')
-def link():
-    
+@app.route('/steam/auth')
+def auth_steam():
+    hostname = "http://"+env["webserver"]["hostname"]+":"+str(env["webserver"]["port"])
+    steamLogin = SteamSignIn()
+    return steamLogin.RedirectUser(steamLogin.ConstructURL(f"{hostname}/steam"))
+  
 
-    return render_template('views/link.html', discord_url = env["oauth_discord"]["sign_in_url"])
+@app.route('/steam')
+def steam():
+    user = request.args.get("openid.identity")
+    if user:
+        parts = user.split("/")
+        id = parts[len(parts) -1 ]
+        resp = make_response(redirect("/link"))
+        resp.set_cookie("steam_id", value=id)
+
+        return resp 
+    else:
+        return render_template("views/steam.html", steam_url="/steam/auth")
+
+
+@app.route('/link')
+async def link():
+    steam_id = request.cookies.get("steam_id")
+    discord_id = request.cookies.get("discord_id")
+    discord_username = request.cookies.get("discord_username")
+
+
+    await webhook.new_user_added(discord_id, discord_username, steam_id)
+    return redirect("/")
 
 
 
