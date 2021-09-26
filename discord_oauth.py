e50be0f5ea2423d2e526487632e57d748fe8231f
johannes <johannes@lgjt.xyz>
2021-09-26 18:24:07 +0200

    can now get the discord user id

--- discord_oauth.py
+++ discord_oauth.py
@@ -1,6 +1,42 @@
 import json
+import requests
+from werkzeug.datastructures import Authorization
+
+from werkzeug.utils import redirect
+from werkzeug.wrappers import request
 env = json.load(open("env.json"))
 
+CLIENT_ID = env["oauth_discord"]["client_id"]
+SECRET = env["oauth_discord"]["client_secret"]
+DISCORD_API_BASE_URL = "https://discord.com/api"
+DISCORD_SIGN_IN_URL = env["oauth_discord"]["sign_in_url"]
+DISCORD_REDIRECT_URI = env["oauth_discord"]["redirect_uri"]
+DISCORD_OAUTH_SCOPE = env["oauth_discord"]["scope"]
+DISCORD_TOKEN_URL = "https://discord.com/api/oauth2/token"
 
-DISCORD_API_BASE_URL = "https://discordapp.com/api"
-DISCORD_REDIRECT_URI = env["webserver"]["hostname"]+":"+str(env["webserver"]["port"])
+
+def get_access_token(code):
+    data = {
+        "client_id": CLIENT_ID,
+        "client_secret": SECRET,
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": DISCORD_REDIRECT_URI,
+        "scope": DISCORD_OAUTH_SCOPE
+    }
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    req = requests.post(f"{DISCORD_API_BASE_URL}/oauth2/token", data=data, headers=headers)
+
+    return req.json()
+
+def get_user(access_token):
+
+    headers = {
+        "Authorization": "Bearer {}".format(access_token)
+    }
+
+    req = requests.get(f"{DISCORD_API_BASE_URL}/users/@me", headers=headers)
+
+    return req.json()
--- webserver.py
+++ webserver.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
 
 import json
+import requests
+import discord_oauth
+
+from werkzeug.wrappers import response
 app = Flask(__name__, static_folder='static')
 env = json.load(open("env.json"))
 
@@ -12,9 +16,23 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/discord')
+def discord():
+    code = request.args.get("code")
+    if code:
+        token = discord_oauth.get_access_token(code)["access_token"]
+        user = discord_oauth.get_user(token)
+
+        return user
+    else:
+        return render_template("views/discord.html", discord_url = env["oauth_discord"]["sign_in_url"])
+
 @app.route('/link')
 def link():
-    return render_template('views/link.html')
+    
+
+    return render_template('views/link.html', discord_url = env["oauth_discord"]["sign_in_url"])
+
 
 
 if __name__ == "__main__":
